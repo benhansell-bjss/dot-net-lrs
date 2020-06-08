bfbc148afd05327bc1d3c53e3a6ba9d3a193203b
bhansell <ben_hansell@live.co.uk>
2020-06-08 11:55:58 +0100

    reverting changes to activity controller.

--- src/WebUI/ExperienceApi/Controllers/ActivitiesController.cs
+++ src/WebUI/ExperienceApi/Controllers/ActivitiesController.cs
@@ -4,7 +4,6 @@ using Doctrina.WebUI.ExperienceApi.Mvc.Filters;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,24 +19,6 @@ namespace Doctrina.WebUI.ExperienceApi.Controllers
         public ActivitiesController(IMediator mediator)
         {
             _mediator = mediator;
-        }
-
-        [HttpGet("{id}")]
-        [HttpHead]
-        public async Task<ActionResult> GetActivityDocumentAsync([FromQuery]Guid id, CancellationToken cancelToken = default)
-        {
-            var command = new GetActivityQuery() { ActivityId = new Iri(id.ToString()) };
-
-            Activity activity = await _mediator.Send(command, cancelToken);
-
-            if (activity == null)
-            {
-                return Ok(new Activity());
-            }
-
-            // TODO: Return only canonical that match accept-language header, or und
-
-            return Ok(activity);
         }
 
         [HttpGet]
